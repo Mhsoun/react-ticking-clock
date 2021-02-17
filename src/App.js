6351e53f00d7361c646c7ca64baf06ca53ce605c
mhsoun <m.mhsoon@gmail.com>
2021-02-17 10:29:05 +0100

    Added ticking clock

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import React from 'react';
+
+class Clock extends React.Component{
+  constructor(props){
+    super(props);
+    this.state= {
+    date: new Date()
+    };
+  }
+
+  componentDidMount(){
+    this.timerID = setInterval(()=> this.setState({date: new Date()}), 1000);
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.timerID);
+  }
+  render(){
+    return(
+      <>
+      <h1>Simply time now </h1>
+    <h1>Time now {this.state.date.toLocaleTimeString()}</h1>
+    </>
+    )}
+}
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Clock />
+    </div>
+  );
+}
+
+export default App;
